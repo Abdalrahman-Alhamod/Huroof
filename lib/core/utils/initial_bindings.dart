52a338142da0f230a607d8177387e95326ad7aca
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2025-10-29 00:15:26 +0300

    convert to local datasource

--- lib/core/utils/initial_bindings.dart
+++ lib/core/utils/initial_bindings.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:huroof/app/data/source/letters_data_source.dart';
-import 'package:huroof/app/data/source/letters_remote_data_source.dart';
 
+import '../../app/data/source/letters_local_data_source.dart';
 import '../services/api_services.dart';
 import '../services/api_services_impl.dart';
 
@@ -11,7 +11,7 @@ class InitialBindings extends Bindings {
   void dependencies() {
     Get.lazyPut<Dio>(() => Dio());
     Get.lazyPut<ApiServices>(() => ApiServicesImpl());
-    Get.lazyPut<LettersDataSource>(() => LettersRemoteDataSource());
-    // Get.lazyPut<LettersDataSource>(() => LettersLocalDataSource());
+    // Get.lazyPut<LettersDataSource>(() => LettersRemoteDataSource());
+    Get.lazyPut<LettersDataSource>(() => LettersLocalDataSource());
   }
 }
